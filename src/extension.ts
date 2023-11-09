80a65b90536493682f52cd4042cdfa77c5e567dc
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-09 19:56:35 +0200

    squash!

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
-import * as vscode from 'vscode';
-import SpellProvider from './features/spellProvider';
+import * as vscode from "vscode";
+import SpellProvider from "./features/spellProvider";
 
 export function activate(context: vscode.ExtensionContext): void {
 	let linter = new SpellProvider();
